41db124464e8f7e26d342722516b8412cede0f5c
prev1ew <warcied1@gmail.com>
2022-05-05 05:29:20 -0400

    частично доработанный механизм (есть часть механизма не рабочая, которая в процессе доработки)

--- VKinder/main.py
+++ VKinder/main.py
@@ -1,1 +1,50 @@
-# основной интерфейс программы
+import time
+import vk
+import listener
+from threading import Thread
+
+# инициалиция
+vk_client = vk.initialize_vk_client()
+longpoll = vk.get_longpoll_from_vk(vk_client)
+
+# это нужно для того чтобы указывать в редирект адресс нужный адресс на котором у нас запущен сервис
+COMPUTER_IP = vk.get_ip()
+PORT = '8008'
+
+# запускаем сервера с целью возможности получения токена от пользователя
+# TODO: доработать механизм обработки токена (в слушателе)
+th = Thread(target=listener.run_listener)
+th.start()
+
+#
+while True:
+    time.sleep(5)
+    for event in longpoll.listen():
+        if vk.is_event_equal_new_message(event.type):
+            # TODO:получить данные юзера (из бд)
+            user_data_existed = False
+            if user_data_existed:
+                pass
+            else:
+                user_data = vk.get_user_data(vk_client, event.user_id)
+                # TODO: записать данные в бд
+
+            if event.to_me:
+                request = event.text.lower()
+
+                if request == "привет":
+                    vk.write_msg(vk_client, event.user_id, f"Хай, {event.user_id}")
+                elif request == "пока":
+                    vk.write_msg(vk_client, event.user_id, "Пока((")
+                elif request == "ищи людей":
+                    # TODO: проверить ли есть токен в бд
+                    # TODO: получить токен
+                    # TODO: или сформировать новый если нет
+
+                    # поиск людей не работает так как нужен не коммьюнити токен
+                    # TODO: доработать после доработки токенов
+                    result = vk.search_people(user_data)
+                    vk.write_msg(vk_client, event.user_id, "Пока((")
+                else:
+                    # TODO: написать хелп если некорректный ввод
+                    vk.write_msg(vk_client, event.user_id, "Не поняла вашего ответа...")
--- VKinder/vk.py
+++ VKinder/vk.py
@@ -1,28 +1,82 @@
-# Взаимодействие с VK API
+from datetime import date
 from random import randrange
-
+import time
+import requests
+from flask import Flask, request
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-
-token = input('Token: ')
-
-vk = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vk)
+import socket
+from threading import Thread
+import listener
 
 
-def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
 
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
+def initialize_vk_client():
+    group_token = '0958750174482253c31483e132a96c88aa890529dfe797f60e04beb97f8522441c78629e31f280bcd644c'
+    return vk_api.VkApi(token=group_token)
 
-        if event.to_me:
-            request = event.text
 
-            if request == "привет":
-                write_msg(event.user_id, f"Хай, {event.user_id}")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
-            else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+def get_longpoll_from_vk(vk):
+    return VkLongPoll(vk)
+
+
+def write_msg(vk, user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7)})
+
+
+def calc_user_age(bdate):
+    list_date = bdate.split('.')
+    today = date.today()
+    age = today.year - int(list_date[2]) - 1
+    if today.month > int(list_date[1]) and today.day > int(list_date[0]):
+        age += 1
+    return age
+
+
+def get_user_data(vk, user_id):
+    res = vk.method('users.get', {'user_ids': user_id, 'fields': 'bdate, city, sex'})
+    info = dict()
+    info['gender'] = 'M' if res[0]['sex'] == 2 else 'W'
+    info['city'] = res[0]['city']
+    info['age'] = calc_user_age(res[0]['bdate'])
+    info['id'] = user_id
+    return info
+
+
+def select_age(info):
+    age = info[2]
+    if info[0] == 1:
+        age_from = age
+        age_to = age + 3
+    else:
+        age_from = age - 3
+        age_to = age
+    return [age_from, age_to]
+
+
+def search_people(vk, info):
+    age_from_to = select_age(info)
+    res = vk.method('users.search', {
+        'hometown': info.city,
+        # 1 = mens, 2 = women
+        'sex': 1 if info.gender == 'M' else 2,
+        'age_from': age_from_to[0],
+        'age_to': age_from_to[1]
+    })
+    return res
+
+
+def is_event_equal_new_message(event_type):
+    return event_type == VkEventType.MESSAGE_NEW
