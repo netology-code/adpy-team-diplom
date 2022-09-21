e2711f2fef595f389c631f321d55e655d17e6206
efabritz <katya-belova@inbox.ru>
2022-09-21 19:43:49 +0200

    db sql file  version 1

---
+++ v1/v1db.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS public.User(
+id SERIAL PRIMARY KEY,
+name VARCHAR(40) NOT NULL,
+surname VARCHAR(40) NOT NULL,
+b_date DATE NOT NULL DEFAULT CURRENT_DATE,
+gender VARCHAR(20) NOT NULL,
+profile_link VARCHAR(60) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS public.UserFoto(
+id SERIAL PRIMARY KEY,
+user_id INTEGER NOT NULL REFERENCES public.User(id),
+foto_link VARCHAR(60) NOT NULL UNIQUE,
+likes INTEGER NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS public.PossiblePair(
+id SERIAL PRIMARY KEY,
+user_id INTEGER NOT NULL REFERENCES public.User(id),
+pair_id INTEGER NOT NULL REFERENCES public.User(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.UserFavouritList(
+id SERIAL PRIMARY KEY,
+user_id INTEGER NOT NULL REFERENCES public.User(id),
+favourite_id INTEGER NOT NULL REFERENCES public.User(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS public.UserBlackList(
+id SERIAL PRIMARY KEY,
+user_id INTEGER NOT NULL REFERENCES public.User(id),
+blocked_id INTEGER NOT NULL REFERENCES public.User(id)
+);
+
+
