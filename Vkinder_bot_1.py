0ed8c1f9b83bbbe6eb8c4a1abcd57492f6fbea55
EZ <dw04@ya.ru>
2023-04-13 18:59:50 +0300

    Актуализация, вывод избр. из БД

--- Vkinder_bot_1.py
+++ Vkinder_bot_1.py
@@ -10,9 +10,11 @@ from work_with_db import VKinderDB
 
 load_dotenv(find_dotenv())
 token = os.getenv('ACCESS_TOKEN')
-input_data_list = []    #список входных данных полученных от пользователя для поиска
-user_id_list = []   #значение user_id
-chosen_candidate_list = []  #список избранных кандидатов
+input_data_list = []  # список входных данных полученных от пользователя для поиска
+user_id_list = []  # значение user_id
+intermidiate_list = []  # список временного хранения кандидатов
+chosen_candidate_list = []  # список избранных кандидатов
+
 
 # генерация клавиатуры чат-бота
 def menu_keyboard():
@@ -20,14 +22,17 @@ def menu_keyboard():
     keyboard.add_button(label='\U00002605', color=VkKeyboardColor.PRIMARY)
     keyboard.add_button(label='\U0000279C', color=VkKeyboardColor.POSITIVE)
     keyboard.add_line()
-    keyboard.add_button(label='Показать \U00002605', color=VkKeyboardColor.SECONDARY)
+    keyboard.add_button(label='показать \U00002605',
+                        color=VkKeyboardColor.SECONDARY)
     return keyboard.get_keyboard()
 
 
 # отправка сообщений в чат-бот
 def write_msg(user_id, message, attachment=None, keyboard=None):
-    vk_session.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7),
-                                        'attachment': attachment, 'keyboard': keyboard})
+    vk_session.method('messages.send',
+                      {'user_id': user_id, 'message': message,
+                       'random_id': randrange(10 ** 7),
+                       'attachment': attachment, 'keyboard': keyboard})
 
 
 # получение команд от пользователя в чат-боте
@@ -42,29 +47,31 @@ def new_message(candidate_data=None):
                     write_msg(event.user_id,
                               'Добро пожаловать в Vkinder!\n\n'
                               'Введите данные для поиска через запятую в формате:\nВозраст, пол, город:')
-                    # chosen_candidate_list.clear()
                 elif request.lower() == 'стоп':
                     break
                 elif candidate_data is not None:
                     if request.lower() == '\U0000279C':
-                        generator_candidates(next(candidate_data))
+                        intermidiate_list.append(
+                            generator_candidates(next(candidate_data)))
                     elif request.lower() == '\U00002605':
-                        vk_db = VKinderDB(next(candidate_data))
+                        vk_db = VKinderDB(intermidiate_list.pop())
                         vk_db.add_favorite()
                     elif request.lower() == 'показать \U00002605':
-                        pass
+                        vk_db = VKinderDB({})
+                        favorites = vk_db.get_all_favorite()
+                        for candidate in favorites:
+                            generator_candidates(candidate)
                     else:
                         write_msg(user_id_list[0], 'Неопознанная команда!')
                 else:
                     if len(check_len_request) == 3:
                         input_data_list.append(request.title().strip())
-                        return input_data_list
-                    elif (request.lower() == '\U0000279C' or '\U00002605') and len(check_len_request) == 0:
                         write_msg(event.user_id,
-                                  'Добро пожаловать в Vkinder!\n\n'
-                                  'Введите данные для поиска через запятую в формате:\nВозраст, пол, город:')
+                                  'Для продолжения нажмите \U0000279C')
+                        return input_data_list
                     else:
-                        write_msg(event.user_id, 'Введен неверный формат данных! Повторите попытку:')
+                        write_msg(event.user_id,
+                                  'Введен неверный формат данных! Повторите попытку:')
 
 
 # форматирование входных данных, генерация словаря данных для поиска кандидатов
@@ -72,7 +79,8 @@ def formatting_data(data_list_):
     join_data_list = ','.join(data_list_)
     pattern_data_list = r'(\d+)\s*\W*([Муж|Жен]+)\w+\W*(\w+)'
     substitution_data_list = r'\1,\2,\3'
-    format_data_list = re.sub(pattern_data_list, substitution_data_list, join_data_list, re.A)
+    format_data_list = re.sub(pattern_data_list, substitution_data_list,
+                              join_data_list, re.A)
     split_format_data_list = format_data_list.split(',')
     data_dict = {'age': int(split_format_data_list[0]),
                  'sex': split_format_data_list[1],
@@ -108,6 +116,7 @@ def generator_candidates(candidate_dict):
                                  f'photo{candidate_dict["id"]}_'
                                  f'{candidate_dict["photos_ids"][1]}',
                       keyboard=menu_keyboard())
+            return candidate_dict
         elif len(candidate_dict['photos_ids']) == 1:
             write_msg(user_id_list[0], f'{candidate_dict["first_name"]} '
                                        f'{candidate_dict["last_name"]}'
@@ -115,21 +124,25 @@ def generator_candidates(candidate_dict):
                       attachment=f'photo{candidate_dict["id"]}'
                                  f'_{candidate_dict["photos_ids"][0]}',
                       keyboard=menu_keyboard())
+            return candidate_dict
         elif len(candidate_dict['photos_ids']) == 0:
             write_msg(user_id_list[0], f'{candidate_dict["first_name"]} '
                                        f'{candidate_dict["last_name"]}'
                                        f'\n{candidate_dict["link"]}',
                       keyboard=menu_keyboard())
+            return candidate_dict
         else:
-            write_msg(user_id_list[0], 'По вашему запросу ничего не найдено!')
+            write_msg(user_id_list[0],
+                      'По вашему запросу ничего не найдено!')
 
 
 if __name__ == '__main__':
+    token = 'vk1.a.Ghab-7n9GgMY3FCHRG1xOk1BmXKwx4xlNisj0iwWMwAt2su9XZdbAgcfUfDZOelzYQsyqec6OabrJaQMWM_q6yaSLiYhKEdOtpBleAupbC8HZngq0kviF3u3RlTbQpidDxx-UzXMrM47vCUUm_e6rX5hwX8awi2VJzQw7z0Z8JXv1VRF-dBumUMRlvJXF_7NytvJjP5t0HD76vJzU7Ue5A'
     vk_session = vk_api.VkApi(token=token)
     longpoll = VkLongPoll(vk_session)
     vk_search = vk_api_data.VKAPI()
     candidate_data = formatting_data(new_message())
-    candidates = vk_search.search_candidates(candidate_data['age'], candidate_data['sex'], candidate_data['city'])
-    generator_candidates(next(candidates))
+    candidates = vk_search.search_candidates(candidate_data['age'],
+                                             candidate_data['sex'],
+                                             candidate_data['city'])
     new_message(candidates)
-    print(chosen_candidate_list)
