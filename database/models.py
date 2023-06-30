5fb9b374683c50b2eb4a69a328f84d375a8ba7b7
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2023-06-30 15:21:27 +0400

    Update models.py add PK

--- database/models.py
+++ database/models.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "user"
 
-    user_id = sq.Column(sq.Bigint, primary_key=True)
+    user_id = sq.Column(sq.BigInteger, primary_key=True)
     user_name = sq.Column(sq.String(length=50), unique=True)
     search_gender = sq.Column(sq.String(length=1), nullable=False)
     search_age = sq.Column(sq.Integer, nullable=False)
@@ -16,23 +16,26 @@ class User(Base):
 class Favorite(Base):
     __tablename__ = "favorite"
 
-    user_id = sq.Column(sq.Bigint, sq.ForeignKey("user_id"), nullable=True)
-    favorite_vk_user_id = sq.Column(sq.Bigint, nullable=True)
+    favorite_id = sq.Column(sq.Integer, primary_key=True)
+    user_id = sq.Column(sq.BigInteger, sq.ForeignKey("user_id"), nullable=False)
+    favorite_vk_user_id = sq.Column(sq.BigInteger, nullable=True)
 
     favorite = relationship(User, backref="user")
 
 class Blacklist(Base):
     __tablename__ = "blacklist"
 
-    user_id = sq.Column(sq.Bigint, sq.ForeignKey("user_id"), nullable=True)
-    blocked_vk_user_id = sq.Column(sq.Integer)
+    blacklist_id = sq.Column(sq.Integer, primary_key=True)
+    user_id = sq.Column(sq.BigInteger, sq.ForeignKey("user_id"), nullable=False)
+    blocked_vk_user_id = sq.Column(sq.Integer, nullable=True)
 
     user = relationship(User, backref="user")
     
 class State(Base):
     __tablename__ = "state"
 
-    user_id = sq.Column(sq.Bigint, sq.ForeignKey("user_id"), nullable=True)
+    state_id = sq.Column(sq.Integer, primary_key=True)
+    user_id = sq.Column(sq.BigInteger, sq.ForeignKey("user_id"), nullable=False)
     state = sq.Column(sq.Integer)
 
     user = relationship(User, backref="user")
