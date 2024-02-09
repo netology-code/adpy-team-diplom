272888fb367e36a799895aa555532245643eb396
Warswat <warswat228@gmail.com>
2024-02-09 19:53:18 +0300

    работает с БД немного

--- basic_code.py
+++ basic_code.py
@@ -6,23 +6,24 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor, VkKeyboardButton
 from sqlalchemy import create_engine
 from db.orm import ORMvk
 from db.create_db import create_tables
+from datetime import date
 
-DSN = f'postgresql://postgres:201224@localhost:5432/vkinder'
+DSN = f'перенёс временно из main'
 
 engine = create_engine(DSN)
 
 create_tables(engine)
 
-personal_token = 'vk1.a.N7DJW9j7Q9TLUBuzGdFHNKrEaHc1S3VUSeRdC9a-v11Cltud98VuO3gLoBhMTr0QGUNYWHiKqViavFSRmS_bkRXYNoCXDXfB6eDJZuMyhwa5-QpzjTswaPmCzLgQDCn5M-BMO3LOT_PtMGOs2xQzy0IxMUHjdVds9dBDT2fNUdt-5CIaSIlUHGDcThD-lhjR'
+personal_token = 'токен'
 myORM = ORMvk(engine)
 def filter_friends(partners_list):
-    if not partners_list['is_friend'] and not partners_list['is_closed']:
+    if not partners_list['is_friend'] and not partners_list['is_closed'] and len(partners_list['bdate']) > 8:
         return True
     else:
         return False
 
 personal_vk = vk_api.VkApi(token=personal_token)
-token = 'vk1.a.tJsBcxT5vZEq8eIh-VMmuhJDeWdEoUm5Ziz8Mg524f-0CSIW7Bi4xXkHQLUeBdhXuv6n8A19umgV3onwGodtHrxnGt6mrvhjQQOPWPUSBrbbC0mgL9IKPmYJHsD1CS8B_7NRW9d5qabSxG3AAsHEbdj7iRGFFrXEDEQE1gocE2YYj9iLbby8RFI3-Nwflocrd9llPrW1k6EvLW774A-FpA'
+token = 'токен'
 # token = input('Token: ')
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
@@ -32,6 +33,9 @@ active_keyboard = VkKeyboard()
 
 testers = None
 
+def partners_gen(partners_list):
+    for person in partners_list:
+        yield person
 
 class VkBot:
     def __init__(self, personal_vk_token, vk_token):
@@ -42,23 +46,32 @@ class VkBot:
         self.hometown = ''
         self.partner_age = 0
 
-    def next_partner(self):
-        if self.testers is None or not self.testers:
-            self.testers = \
-            self.personal_vk.method('users.search',
-                                    values={'count': 100, 'sex': 1, 'has_photo': 1,'hometown': self.hometown,'age_from': self.partner_age-1,'age_to': self.partner_age+1, 'fields': ['is_friend']})['items']
-        self.testers = list(filter(filter_friends,self.testers))
+    def get_photos(self, person):
         all_photos = []
         for image in self.personal_vk.method('photos.get',
-                                   {'owner_id': self.testers[0]['id'],
-                                    'album_id': "profile", "extended": "1"})['items']:
+                                             {'owner_id': person['id'],
+                                              'album_id': "profile", "extended": "1"})['items']:
             all_photos.append([image["likes"]["count"], image["id"]])
         all_photos.sort()
         all_photos.reverse()
         best_images = []
         for image in all_photos[:3]:
             best_images.append(f"photo{self.testers[0]['id']}_{image[1]}")
-        self.send_photos(event.user_id, attachment=",".join(best_images))
+        return best_images
+
+    def next_partner(self):
+        if self.testers is None or not self.testers:
+            self.testers = \
+            self.personal_vk.method('users.search',
+                                    values={'count': 1000, 'sex': 1, 'has_photo': 1,'hometown': self.hometown,'age_from': self.partner_age-2,'age_to': self.partner_age+2, 'fields': 'is_friend, sex, bdate'})['items']
+        self.testers = list(filter(filter_friends,self.testers))
+        for person in partners_gen(self.testers):
+            print(person)
+            myORM.add_partner(event.user_id, person['id'],
+                              {'name': person['first_name'], 'surname': person['last_name'],
+                               'gender': person['sex'],
+                               'age': int(str(date.today())[:4]) - int(person['bdate'][-4:]), 'foto': self.get_photos(person), 'link': f'https://vk.com/id{person["id"]}'})
+        # self.send_photos(event.user_id, attachment=",".join(self.get_photos()))     брать из БД будем
         self.write_msg(event.user_id, f'https://vk.com/id{self.testers[0]["id"]}')
         self.testers.pop(0)
 
@@ -114,15 +127,9 @@ def send_keyboard(user_id, message, keyboard):
 
 first_keyboard.add_button('Подобрать', VkKeyboardColor.PRIMARY)
 first_keyboard.add_button('Автоподбор', VkKeyboardColor.PRIMARY)
-#vkkeyboard.add_button('Следующий', VkKeyboardColor.PRIMARY)
 first_keyboard.add_line()
-#vkkeyboard.add_button('В избранное', VkKeyboardColor.PRIMARY)
 first_keyboard.add_button('Показать избранное', VkKeyboardColor.PRIMARY)
-#vkkeyboard.add_button('Заблокировать', VkKeyboardColor.PRIMARY)
-#vkkeyboard.add_line()
 first_keyboard.add_button('Показать заблокированных', VkKeyboardColor.PRIMARY)
-#vkkeyboard.add_button('Лайк', VkKeyboardColor.PRIMARY)
-#vkkeyboard.add_button('Дизлайк', VkKeyboardColor.PRIMARY)
 
 active_keyboard.add_button('Следующий', VkKeyboardColor.PRIMARY)
 active_keyboard.add_button('Назад', VkKeyboardColor.PRIMARY)
